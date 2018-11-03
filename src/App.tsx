4db0f5269355fda0843da154ffece8de56f6b0e1
YannickLeRoux <rasyann@hotmail.com>
2018-11-02 17:04:24 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+var promise = require('redux-promise').default;
+
+import { BrowserRouter } from 'react-router-dom';
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+const store:any = createStore(reducers, applyMiddleware(promise));
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+          <NavBar />
+          Test
+          </div>
+
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
